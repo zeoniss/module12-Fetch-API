c5f5038e7954f021dadd5c572226b132301fd060
Vladimir Kriukov <zeoniss28@gmail.com>
2021-02-13 17:30:19 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,2 @@
-import './sass/styles.scss';
-import fetchArticles from './js/fetch-articles';
-import updateArticlesMarkup from './js/update-articles-markup';
-import refs from './js/refs';
-
-refs.searchForm.addEventListener('submit', event => {
-  event.preventDefault();
-
-  const form = event.currentTarget;
-  const inputValue = form.elements.query.value;
-
-  refs.articlesContainer.innerHTML = '';
-  form.reset();
-
-  fetchArticles(inputValue).then(updateArticlesMarkup);
-});
+//
+fetch('https://jsonplaceholder.typicode.com/todos')
