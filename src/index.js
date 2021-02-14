4eda47acb1b2a849ff2fa2ef4a5aaef86202b6f6
Vladimir Kriukov <zeoniss28@gmail.com>
2021-02-14 11:53:08 +0200

    summary

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-//
-fetch('https://jsonplaceholder.typicode.com/todos')
+import './sass/styles.scss';
+import fetchArticles from './js/fetch-articles';
+import updateArticlesMarkup from './js/update-articles-markup';
+import refs from './js/refs';
+
+refs.searchForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const inputValue = form.elements.query.value;
+
+    refs.articlesContainer.innerHTML = '';
+    form.reset();
+
+    fetchArticles(inputValue).then(updateArticlesMarkup);
+});
